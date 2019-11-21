01463f44b8f27c6cb27daa9c58ef5a4435d16eae
Andre <agmgomes94@gmail.com>
2019-11-21 00:37:38 +0000

    logger added

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const morgan = require('morgan');
 
 const app = express();
+
+let accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {
+  flags: 'a'
+});
 
 // BodyParser Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Logger Middleware
+app.use(morgan('combined', { stream: accessLogStream }));
 
 const port = process.env.PORT || 5000;
 
